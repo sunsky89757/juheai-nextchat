b310d8090a9c804c81a6eb82fdb8bd6d27482723
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2024-01-20 10:49:59 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -359,7 +359,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "欢迎免费使用聚合AI极速GPT-3.5全系列，如需GPT-4或更多模型请[注册/登录](https://api.juheai.top/)领取API-Key后使用",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
